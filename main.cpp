483e9895fb7b8e8d5059e0c20bbe958cef74a541
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-02-06 14:00:01 +0200

    Added pifagor

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int main()
     int a{7}, b{6};
     std::cout << "Hello, world!";
     std::cout << pow(a, b) << std::endl;
+    std::cout << std::sqrt(pow(a,2) + pow(b,2)) << std::endl;
     std::cout << a + b << std::endl;
     std::cout << a - b << std::endl;
     std::cout << multiply(a, b) << std::endl;
