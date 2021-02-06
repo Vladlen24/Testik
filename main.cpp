7b23ae7b036bb048cf9e394f70cdcc84be561249
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-02-06 11:16:40 +0200

    Added f & output a*b

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int multiply(int a, int b) {
     return a * b;
 }
 
+void f() {}
+
 int main()
 {
     int a{7}, b{6};
@@ -13,5 +15,6 @@ int main()
     std::cout << pow(a, b) << std::endl;
     std::cout << a + b << std::endl;
     std::cout << a - b << std::endl;
+    std::cout << multiply(a, b) << std::endl;
     return 0;
 }
