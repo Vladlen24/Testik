d8d8e8da5074bc1ec9f0c2e7ddd4e7ff96f32e13
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-02-06 10:57:54 +0200

    Added pow(a, b)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 int main()
 {
     int a{7}, b{6};
     std::cout << "Hello, world!";
-    
+    std::cout << pow(a, b) << std::endl;
     std::cout << a + b << std::endl;
     std::cout << a - b << std::endl;
     return 0;
