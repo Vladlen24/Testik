67917fea87a4aae1602337d9e64871705fbae1f3
Vladlen24 <55432082+Vladlen24@users.noreply.github.com>
2021-02-06 10:43:09 +0200

    Update main.cpp
    Deleted buy & Added a-b

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ int main()
 {
     int a{7}, b{6};
     std::cout << "Hello, world!";
-    std::cout << " Buy an elephant!";
+    
     std::cout << a + b << std::endl;
+    std::cout << a - b << std::endl;
     return 0;
 }
