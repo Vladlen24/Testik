b821e827120c9585956aacc5aa269157290f2021
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-02-07 21:42:11 +0200

    Added chronometr

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <chrono>
+#include <vector>
+#include <algorithm>
 
-int multiply(int a, int b) {
+/*int multiply(int a, int b) {
 
     return a * b;
 }
@@ -12,10 +15,31 @@ int divide(int a, int b) {
 }
 
 void f() {}
+*/
+
+class Timer
+{
+public:
+    using clock_t = std::chrono::steady_clock;
+    using time_point_t = clock_t::time_point;
+    std::chrono::milliseconds interval_pause{100};
+    Timer(): m_begin(clock_t::now()) {}
+    ~Timer()
+    {
+        auto end = clock_t::now();
+        std::cout << "milliseconds: " << std::chrono::duration_cast <std::chrono::milliseconds> (end - m_begin).count() - interval_pause.count() << std::endl;
+    }
+    /*void pause()
+    {
+        ++interval_pause;
+    }*/
+private:
+    time_point_t m_begin;
+};
 
 int main()
 {
-    int a{7}, b{6};
+    /*int a{7}, b{6};
     std::cout << "Hello, master";
     std::cout << pow(a, b) << std::endl;
     std::cout << std::sqrt(pow(a,2) + pow(b,2)) << std::endl;
@@ -23,13 +47,25 @@ int main()
     std::cout << a - b << std::endl;
     --a;
     ++b;
-    std::cout << multiply(a, b) << std::endl;
+    std::cout << multiply(a, b) << std::endl;*/
 
     //при использовании Merge мы подгружали всю историю коммитов. Благодаря этому сохраняется детальное описание
     //всех действий, но если коммитов в ветке было сделано сильно много (или паралельно в нескольких ветках работают
     //несколько разработчиков), то Log нагромаждается. Для простоты и чистоты Loga вместо Merge используем Rebase.
     //Тогда в Loge видим только одну петельку, которая отображает перемещение из новой ветки в ветку мастера (даже
     //если в новой ветке много комитов). Причем эта петелька поднимается выше последнего коммита
+
+    std::vector v (1000000, 0);
+
+    for (auto i = 0; i < std::size(v); ++i)
+    {
+        v[i] = 1000000 - i;
+    }
+
+    Timer t;
+    std::sort(std::begin(v), std::end(v));
+
+    //Timer::pause();
 
     return 0;
 }
