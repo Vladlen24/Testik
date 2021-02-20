deaa6af18ccc048a55f74b0d2d0785e7d7d85e4f
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-02-21 00:04:01 +0200

    Synchr

--- week_1/cmake-build-debug/cmake_install.cmake
+++ week_1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/vladlen/Testik
+# Install script for directory: /home/vladlen/Testik/week_1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/vladlen/Testik/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/vladlen/Testik/week_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
