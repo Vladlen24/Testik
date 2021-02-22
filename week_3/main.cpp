5837a0768368e24399537b3087719a8fd19e2365
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-02-22 21:48:46 +0200

    Task 2 (hash for float/double)

--- week_3/main.cpp
+++ week_3/main.cpp
@@ -30,15 +30,6 @@ private:
     time_point_t m_begin;
 };
 
-std::array<int, 1000000> get_rand_ar() {
-    srand(time(nullptr));
-    std::array<int, 1000000> ar{};
-    for (int& x : ar) {
-        x = rand();
-    }
-    return ar;
-}
-
 std::vector<int> get_rand_v(int n) {
     srand(time(nullptr));
     std::vector<int> v(n);
@@ -48,22 +39,29 @@ std::vector<int> get_rand_v(int n) {
     return v;
 }
 
-std::set<int> get_rand_s(int n) {
-    srand(time(nullptr));
-    std::vector<int> v(n);
-    std::set<int> s;
-    for (int& x : v) {
-        x = rand();
-        s.insert(x);
-    }
-    return s;
+uint64_t hash_for_double(double x) {                                            // хэш-функция для double. Принцип работы переводит 64 бита (8 байтов), которые представляют
+                                                                                // число типа double, в такие же 8 байт только теперь описывающие уже тип int. То есть байты
+    void* temp = &x;                                                            // одни и те же, но в памяти представляются по-разному
+    auto* cast = static_cast<uint64_t *>(temp);
+    auto hash = *cast;
+
+    return hash;
+}
+
+uint32_t hash_for_float(float x) {                                              // хэш-функция для float. Идея и реализация абсолютно такая же, только тут уже 32 бита (4 байта)
+                                                                                // За счет принципа работы этой хэш-функции получается, что она устанавливает взаимно однозначное
+    void* temp = &x;                                                            // соответствие (биекцию) между аргументом и результатом. Поэтому можно считать эту хэш-функцию
+    auto* cast = static_cast<uint32_t *>(temp);                                 // ИДЕАЛЬНОЙ (коллизии отсутствуют). Конечно в учет не брался тот факт, что при превышении допустимой
+    auto hash = *cast;                                                          // точности соответствующего типа хэш-код будет одинаковым. Но тут уже ничего не поделаешь(
+
+    return hash;
 }
 
 int main()
 {
 
     {
-        int n = 2000000;
+        int n = 10000;
         std::vector<int> v = get_rand_v(n);
         {
             std::cout << "set: ";                                               // сложность этого блока иммет ассимптотику O(NlogN); при замерах времени
@@ -91,7 +89,7 @@ int main()
             Timer t;                                                            // при сериях измерений врмени работы выяснилось что этот блок работает в 3.8 раза быстрее блока с set
                                                                                 // {Пример: при 200000 элемент блок отработал за 63 милисекунд}
             srand(time(nullptr));
-            std::array<int, 2000000> ar{};
+            std::array<int, 10000> ar{};
             for (int i = 0; i < v.size(); ++i) {
                 ar[i] = v[i];
             }
@@ -99,6 +97,11 @@ int main()
         }                                                                       // Порогового значения при котором сменяется лидер НЕ ОБНАРУЖЕНО, дошел по количеству элементов
     }                                                                           // уже до того, что памяти не хватало и программа падала, а очередность оставалась прежней
 
+    double x = 5.7;
+    std::cout << hash_for_double(x) << std::endl;                               // результат 4618103647896390861
+
+    float y = 5.7;
+    std::cout << hash_for_float(y) << std::endl;                                // результат 1085695590
 
     return 0;
 }
