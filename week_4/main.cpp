fb45f419928f1e704516753b8e9f6d310616c72b
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-03-02 19:41:37 +0200

    Task 4

--- week_4/main.cpp
+++ week_4/main.cpp
@@ -6,27 +6,32 @@
 #include <algorithm>
 #include <random>
 
-int main() {
-
-    std::vector<int> v(10);
-    std::iota(v.begin(), v.end(), 1);
-
-    //std::vector<std::string> s = {"a", "b", "c", "d"};
-
+void print (std::vector<int> &v, std::string s) {
+    std::cout << s;
     copy(v.cbegin(), v.cend(),
          std::ostream_iterator<int> (std::cout, " "));
+    std::cout << std::endl;
+}
 
+int main() {
+//-----------------------------------------------------TASK 1-----------------------------------------------------------
+    std::vector<int> v(10);
+    std::iota(v.begin(), v.end(), 1);
+    print(v, "Последов: ");
+//-----------------------------------------------------TASK 2-----------------------------------------------------------
     copy(std::istream_iterator<int> (std::cin), std::istream_iterator<int> (),
             back_inserter(v));
-
-    // ПЕРЕТАСОВКА
-
+    print(v, "Вставка: ");
+//-----------------------------------------------------TASK 3-----------------------------------------------------------
     std::default_random_engine dre;        // генератор случайных чисел по умолчанию
     std::shuffle(v.begin(), v.end(),       // интервал
                  dre);                     // источник случайности
-
-    copy(v.cbegin(), v.cend(),
-         std::ostream_iterator<int> (std::cout, " "));
+    print(v, "Перетасовка: ");
+//-----------------------------------------------------TASK 4-----------------------------------------------------------
+    std::vector<int>::iterator pos;
+    pos = std::unique(v.begin(), v.end());
+    v.erase(pos, v.end());
+    print(v, "Без дубликатов: ");
     std::cout << std::endl;
 
     return 0;
