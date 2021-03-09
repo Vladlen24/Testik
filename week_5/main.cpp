cd8b186f6a4e5b80084b7f34c14172606abe77ba
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-03-09 22:59:13 +0200

    Upgraded Conv: RUB -> USD Conv: USD -> RUB

--- week_5/main.cpp
+++ week_5/main.cpp
@@ -1,21 +1,24 @@
-#include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <iterator>
-#include <locale>
-#include <sstream>
+
+const long double KOEF{73};             // коэффициент перевода доллара в рубли
 
 int main()
 {
-    std::string s1;
-    std::cin >> s1;
-    std::istringstream in1(s1);
     long double v1;
-    in1.imbue(std::locale("en_US.UTF-8"));
-    in1 >> std::get_money(v1);
+    std::cin.imbue(std::locale("en_US.UTF-8"));                                         // конвертация долларов в рубли
+    std::cin >> std::get_money(v1,false);
     std::cout.imbue(std::locale("ru_RU.UTF-8"));
-    std::cout << std::showbase << "RUB -> USD: " << std::put_money(v1 * 73)
-              << " or " << std::put_money(v1 * 73, true) << '\n';
+    std::cout << std::showbase << "USD -> RUB: " << std::put_money(v1 * KOEF)
+              << " or " << std::put_money(v1 * KOEF, true) << '\n';
+
+    long double v2;
+    std::cin.imbue(std::locale("ru_RU.UTF-8"));                                         // конвертация рублей в доллары
+    std::cin >> std::get_money(v2, false);
+    std::cout.imbue(std::locale("en_US.UTF-8"));
+    std::cout << std::showbase << "RUB -> USD: " << std::put_money(v2 / KOEF)
+              << " or " << std::put_money(v2 / KOEF, true) << '\n';
+
 
     return 0;
 }
