261986d4ffba51ec61f25c69dfc81406ea8277e3
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-03-25 23:54:25 +0200

    task2

--- week_7/main.cpp
+++ week_7/main.cpp
@@ -1,93 +1,42 @@
-#include <iostream>
-#include <cstdlib>
-#include <mutex>
-#include <thread>
-#include <chrono>
-#include <ctime>
-#include <vector>
 #include <algorithm>
-#include <numeric>
-#include <atomic>
-#include <cstdint>
 #include <future>
+#include <iostream>
+#include <numeric>
+#include <thread>
+#include <vector>
 
-static constexpr size_t NUMBER = 8;
-static constexpr size_t THREAD_ITTERS = 40000000;
-
-class Timer
+template < typename Iterator, typename T >
+T parallel_accumulate(Iterator first, Iterator last, T init)
 {
-public:
-    using clock_t = std::chrono::steady_clock;
-    using time_point_t = clock_t::time_point;
+    const std::size_t length = std::distance(first, last);
 
-    Timer(): m_begin(clock_t::now()) {}
+    const std::size_t max_size = 25;
 
-    ~Timer()
+    if (length <= max_size)
     {
-        auto end = clock_t::now();
-        std::cout << std::chrono::duration_cast <std::chrono::milliseconds>(end - m_begin).count() << " milliseconds" << std::endl;
-
+        return std::accumulate(first, last, init);
     }
+    else
+    {
+        Iterator middle = first;
+        std::advance(middle, length / 2);
 
-private:
-    time_point_t m_begin;
-};
+        std::future < T > first_half_result =
+                std::async(parallel_accumulate < Iterator, T >, first, middle, init);
 
-int rrand(int range_min, int range_max) {
-    return rand() % (range_max - range_min + 1) + range_min;
+        T second_half_result = parallel_accumulate(middle, last, T());
+
+        return first_half_result.get() + second_half_result;
+    }
 }
 
-double MonteCarlo(size_t iters) {
+int main(int argc, char ** argv)
+{
+    std::vector < int > v(1000000000);
 
-    srand((unsigned) time(0));
+    std::iota(v.begin(), v.end(), 1);
 
-    unsigned cnt = 0;
-    unsigned side = 1000;
-    unsigned qside = side * side;
+    std::cout << parallel_accumulate(v.begin(), v.end(), 0) << std::endl;
 
-    for (int i = 0; i < iters; ++i) {
-        unsigned x = rrand(0, side);
-        unsigned y = rrand(0, side);
-
-        if ((x * x + y * y) < qside) ++cnt;
-    }
-
-    return double(cnt) / iters * 4;
-}
-
-int main() {
-
-    //паралельная версия
-    {
-        std::cout << "Threads" << std::endl;
-        Timer t;
-        std::vector<double> result;
-        for (int i = 0; i < NUMBER; ++i) {
-            size_t cnt = THREAD_ITTERS;
-            std::future<double> fu = std::async(MonteCarlo, cnt);
-            result.emplace_back(fu.get());
-        }
-        std::cout << std::accumulate(result.begin(), result.end(),0.0) / result.size() << std::endl;
-    }
-
-    //последовательная версия
-    {
-        std::cout << "Iterative" << std::endl;
-        Timer t;
-        std::cout << MonteCarlo(THREAD_ITTERS * NUMBER) << std::endl;
-    }
-
-    //но конкретно в этой программе потоки (паралельн. версия.) работают дольше чем (последов.)
-    //Полагаю это связано с тем что сам по себе алгоритм Монте-Карло достаточно несложный,
-    //но при этом хоть я и запускаю паралельную программу на 8 потоках (по потоку на ядро)
-    //еще работает около 1000 системных потоков. Получается наши вклиниваются между ними и
-    //хоч не хоч, а контекстного переключения не избежать
-    //Т.е. если мы имеем дело с задачей где потоки могли бы выполняять что-то в фоновом режиме
-    //мы бы имели выиграш по врмемни, но в этой программе у нас есть блокровка, так что есть моменты
-    //когда потокам приходится ждать друг друга в порядке "живой очереди" ( прям как в больнице:) )
-    //так как они работают с общей областью памяти
-
-    //но после написания через async и future скорости работы последовательной и итеративной уравнялись с точностью до 0.5%
-
-    return 0;
+    return EXIT_SUCCESS;
 }
--- week_7/task2.cpp
+++ week_7/task2.cpp
@@ -5,38 +5,79 @@
 #include <thread>
 #include <vector>
 
-template < typename Iterator, typename T >
-T parallel_accumulate(Iterator first, Iterator last, T init)
+class Timer
 {
-    const std::size_t length = std::distance(first, last);
+public:
+    using clock_t = std::chrono::steady_clock;
+    using time_point_t = clock_t::time_point;
 
-    const std::size_t max_size = 25;
+    Timer(): m_begin(clock_t::now()) {}
 
-    if (length <= max_size)
+    ~Timer()
     {
-        return std::accumulate(first, last, init);
+        auto end = clock_t::now();
+        std::cout << std::chrono::duration_cast <std::chrono::milliseconds>(end - m_begin).count() << " milliseconds" << std::endl;
+
     }
-    else
-    {
-        Iterator middle = first;
-        std::advance(middle, length / 2);
 
-        std::future < T > first_half_result =
-                std::async(parallel_accumulate < Iterator, T >, first, middle, init);
+private:
+    time_point_t m_begin;
+};
 
-        T second_half_result = parallel_accumulate(middle, last, T());
-
-        return first_half_result.get() + second_half_result;
+template<typename Iterator, typename T>
+struct accumulate_block{
+    void operator () (Iterator first, Iterator last, T& result) {
+        result = std::accumulate(first, last, result);
     }
+};
+
+template<typename Iterator, typename T>
+T parallel_accumulate (Iterator first, Iterator last, T init){
+
+    unsigned long const length = std::distance(first, last);
+    if (!length) {
+        return init;
+    }
+
+    unsigned long const min_per_thread = 25;
+    unsigned long const max_threads = ( length + min_per_thread - 1 ) / min_per_thread;
+    unsigned long const hardware_threads = std::thread::hardware_concurrency();
+    unsigned long const num_threads = std::min ( hardware_threads != 0 ? hardware_threads : 2, max_threads );
+    unsigned long const block_size = length / num_threads;
+
+    std::vector<T> results(num_threads);
+    std::vector<std::thread> threads(num_threads);
+    Iterator block_start = first;
+    for (unsigned long i = 0; i < num_threads; ++i){
+        Iterator block_end = block_start;
+        std::advance(block_end, block_size);
+        threads[i] = std::thread (accumulate_block <Iterator, T> (), block_start, block_end, std::ref(results[i]) );
+        block_start = block_end;
+    }
+    std::for_each (threads.begin(), threads.end(), std::mem_fn (&std::thread::join));
+    return std::accumulate (results.begin(), results.end(), init);
 }
+
+//parallel_accumulate не мое собственное решение, а взято из справочного пособия по паралельным алгоритмам
+//но разница реально очень большая
+//этот код отработал на 10^9 элементов за 2 секунды, причем запустив при этом 12 потоков
+//на 12 ядрах или потоках компа (как это правильно назвать) и все они одновремнно отработали на 100% (это видно на скриншотах)
+//в исходном варианте запустив на таакое же количесво элементов, работали один поток на 100% и еще пару процентов на 20%
+//пару секунд, пока комп просто не завис и пришлось принудительно завершить программу
 
 int main(int argc, char ** argv)
 {
-    std::vector < int > v(100);
+    std::vector < int > v(10000);
 
     std::iota(v.begin(), v.end(), 1);
 
+    Timer t;
     std::cout << parallel_accumulate(v.begin(), v.end(), 0) << std::endl;
 
     return EXIT_SUCCESS;
 }
+
+//(10^6,3), (5*10^6 ,16), (10*10^6,24), (15*10^6,36), (20*10^6,46), (50*10^6,148), (100*10^6,234),
+//(150*10^6,359), (200*10^6,526), (500*10^6,1157), (1000*10^6,2325)
+//построив график видно что зависимость времени выполнения программы линейно зависит от колиества элементов в векторе
+//график прилагается
