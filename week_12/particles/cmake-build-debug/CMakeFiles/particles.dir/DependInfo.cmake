0f096ea9022f0e706ec25e9c1f7872a1fd330935
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2021-05-07 11:44:32 +0300

    particles

---
+++ week_12/particles/cmake-build-debug/CMakeFiles/particles.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vladlen/Testik/week_12/particles/link.cpp" "/home/vladlen/Testik/week_12/particles/cmake-build-debug/CMakeFiles/particles.dir/link.cpp.o"
+  "/home/vladlen/Testik/week_12/particles/main.cpp" "/home/vladlen/Testik/week_12/particles/cmake-build-debug/CMakeFiles/particles.dir/main.cpp.o"
+  "/home/vladlen/Testik/week_12/particles/particle.cpp" "/home/vladlen/Testik/week_12/particles/cmake-build-debug/CMakeFiles/particles.dir/particle.cpp.o"
+  "/home/vladlen/Testik/week_12/particles/pressure.cpp" "/home/vladlen/Testik/week_12/particles/cmake-build-debug/CMakeFiles/particles.dir/pressure.cpp.o"
+  "/home/vladlen/Testik/week_12/particles/system.cpp" "/home/vladlen/Testik/week_12/particles/cmake-build-debug/CMakeFiles/particles.dir/system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SFML_INCLUDE_DIR"
+  "/home/vladlen/Загрузки/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ week_12/particles/cmake-build-debug/CMakeFiles/particles.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/particles.dir/link.cpp.o"
+  "CMakeFiles/particles.dir/main.cpp.o"
+  "CMakeFiles/particles.dir/particle.cpp.o"
+  "CMakeFiles/particles.dir/pressure.cpp.o"
+  "CMakeFiles/particles.dir/system.cpp.o"
+  "particles"
+  "particles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/particles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
